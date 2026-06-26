e33c5e46c84a096e28110f8792c795356eff0c19
Ryan Blajda <ryan.j.blajda@gmail.com>
2026-06-26 09:01:47 -0400

    feat: dynamic title

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { ApiService } from './services/api/api.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('commissioning-checklist-database-manager');
+  protected readonly title = signal('Database Manager');
+  private apiServer = inject(ApiService)
+
+  constructor(private documentTitle:Title) { 
+    this.documentTitle.setTitle(this.title())
+  }
 }
