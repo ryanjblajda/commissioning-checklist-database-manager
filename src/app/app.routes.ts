9697d0d0834ae00c6e4794028775c5707f7f4f7b
Ryan Blajda <ryan.j.blajda@gmail.com>
2026-06-26 09:01:55 -0400

    feat: route config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home-page/home-page';
+import { SuggestionPage } from './suggestion-page/suggestion-page';
+import { AdministrationPage } from './administration-page/administration-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePage },
+    { path: 'suggestion', component: SuggestionPage },
+    { path: 'administration', component: AdministrationPage },
+];
