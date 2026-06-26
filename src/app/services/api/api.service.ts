2f2a89e751b84d6c5a180c88569e94a91392aacd
Ryan Blajda <ryan.j.blajda@gmail.com>
2026-06-26 09:02:50 -0400

    feat: common api service

---
+++ src/app/services/api/api.service.ts
@@ -0,0 +1,58 @@
+// data.service.ts
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MemberPayload } from '../../models/member/member.model';
+import { Observable } from 'rxjs';
+
+// Define an interface for type safety
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'http://127.0.0.1:8000/api/';
+  
+  // Modern way to inject dependencies in Angular
+  private http = inject(HttpClient);
+
+  getCapabilities(): Observable<MemberPayload> {
+    return this.http.get<MemberPayload>(`${this.apiUrl}get/capabilities`)
+  }
+
+  getControlTypes() {
+    return this.http.get<MemberPayload>(`${this.apiUrl}get/controltypes`)
+  }
+
+  getPrefixes() {
+    return this.http.get<MemberPayload>(`${this.apiUrl}get/prefixes`)
+  }
+
+  getPrefix(prefix: string) {
+    return this.http.get<MemberPayload>(`${this.apiUrl}get/prefixes/${prefix}`)
+  }
+
+  getTasks() {
+    return this.http.get<MemberPayload>(`${this.apiUrl}get/tasks`)
+  }
+
+  getModels() {
+    return this.http.get<MemberPayload>(`${this.apiUrl}get/models`)
+  }
+
+  getModel(model: string) {
+    return this.http.get<MemberPayload>(`${this.apiUrl}get/models/${model}`)
+  }
+
+  getManufacturers() {
+    return this.http.get<MemberPayload>(`${this.apiUrl}get/manufacturers`)
+  }
+
+  getManufacturer(manufacturer: string) {
+    return this.http.get<MemberPayload>(`${this.apiUrl}get/manufacturers/${manufacturer}`)
+  }
+}
