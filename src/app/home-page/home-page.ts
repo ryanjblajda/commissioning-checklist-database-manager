bb7dc60949b6734778c2962734bdfcc447771920
Ryan Blajda <ryan.j.blajda@gmail.com>
2026-06-26 09:02:17 -0400

    feat: home page

---
+++ src/app/home-page/home-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Navigation } from '../widgets/navigation/navigation';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [Navigation, RouterLink],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.scss',
+})
+export class HomePage {}
